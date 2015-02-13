6f17e272aa3d8bfb43118a1af5c5ec56d353818d
Rasmus <rasmus.winter@gmail.com>
2015-02-13 11:22:48 +0000

    stack does not exist in ios

--- js/services/sokiLogger.js
+++ js/services/sokiLogger.js
@@ -19,25 +19,35 @@ angular
 					msg = JSON.stringify(msg);
 				}
 				var e = new Error('dummy');
-				var stack = e.stack.replace(/^[^\(]+?[\n$]/gm, '')
-					.replace(/^\s+at\s+/gm, '')
-					.replace(/^Object.<anonymous>\s*\(/gm, '{anonymous}()@')
-					.split('\n');
-				for (var i = 0; i < stack.length; i++) {
-					var regex = /(.+)\s*\((.+):([0-9]+):([0-9]+)\)/g;
-					var args = regex.exec(stack[i]);
-					var fileComponents = args[2].split('/');
-					if (fileComponents[fileComponents.length - 1] != 'sokiLogger.js') {
-						var file = fileComponents.slice(-2).join('/');
-						_service.logs.unshift({
-							date: new Date(),
-							file: file,
-							line: args[3],
-							msg: msg
-						});
-						console.log('SOKI_DEBUG: ' + file + ' line ' + args[3] + ' :: ' + msg);
-						break;
+				if (e.stack) {
+					var stack = e.stack.replace(/^[^\(]+?[\n$]/gm, '')
+						.replace(/^\s+at\s+/gm, '')
+						.replace(/^Object.<anonymous>\s*\(/gm, '{anonymous}()@')
+						.split('\n');
+					for (var i = 0; i < stack.length; i++) {
+						var regex = /(.+)\s*\((.+):([0-9]+):([0-9]+)\)/g;
+						var args = regex.exec(stack[i]);
+						var fileComponents = args[2].split('/');
+						if (fileComponents[fileComponents.length - 1] != 'sokiLogger.js') {
+							var file = fileComponents.slice(-2).join('/');
+							_service.logs.unshift({
+								date: new Date(),
+								file: file,
+								line: args[3],
+								msg: msg
+							});
+							console.log('SOKI_DEBUG: ' + file + ' line ' + args[3] + ' :: ' + msg);
+							break;
+						}
 					}
+				} else {
+					_service.logs.unshift({
+						date: new Date(),
+						file: 'unknown',
+						line: '?',
+						msg: msg
+					});
+					console.log('SOKI_DEBUG: ' + msg);
 				}
 			} catch (ex) {
 				sokiAppUtil.showAlert('Error', ex.message);
